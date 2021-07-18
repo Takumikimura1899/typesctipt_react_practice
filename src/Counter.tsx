6adf6be25c9aa3a18108146c5b390a0cf5a5b5bb
Takumikimura141899 <kimura141899@gmail.com>
2021-07-18 19:58:26 +0900

    useref

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+// const array: Array<number> = [1, 2, 3];
+// const readonlyArray: ReadonlyArray<number> = [1, 2, 3];
+// array[0] = 11;
+// readonlyArray[0] = 11;
 
 // propsを受け取ってないので、{}にしておく
 const Counter: React.FC<{}> = () => {
@@ -21,11 +26,17 @@ const Counter: React.FC<{}> = () => {
     setCount(count - 1);
   };
 
+  const renderTimes = useRef<number>(0);
+  useEffect(() => {
+    renderTimes.current = renderTimes.current + 1;
+  });
+
   return (
     <>
       <div>count:{count}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
+      <div>This component was re-renderdered {renderTimes.current} times</div>
     </>
   );
 };
