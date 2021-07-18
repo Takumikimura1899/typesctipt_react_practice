165afbfc832430f9ba62dd11a7d5a406a06bdfca
Takumikimura141899 <kimura141899@gmail.com>
2021-07-19 05:43:38 +0900

    useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Counter from './Counter';
+import CounterWithReducer from './CounterWithReducer';
 import './App.css';
 
 interface AppProps {
@@ -10,7 +10,7 @@ const App: React.FunctionComponent<AppProps> = ({ message }) => {
   return (
     <>
       <div className='App'>{message}</div>
-      <Counter />
+      <CounterWithReducer />
     </>
   );
 };
